1de8aed5a83b786ebe46c21933cf819396ad8a6e
Johan von Boer <johan.von.boer@umu.se>
2020-03-13 15:07:39 +0100

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # TileMaker
-Creates map tiles to be used as baselayer, based on input image
+
+This program takes an image as input and creates map tiles in a directory structure which can be used as a baselayer in application such as OpenLayers, Cesium and others.
+
+## Things to consider
+* Input image should be high resolution, the higher the better.
+* Input image should be square.
+* Requested zoom level will have a huge impact on processing time and final directory size.
+
+## Output
+* Program will create ´tiles´ directory containing map tiles which can then be served with URL's like: http://example.com/{z}/{x}/{y}.png
+* Output tiles will be scaled to 256x256 pixels.
+
---
+++ TileMaker.py
@@ -0,0 +1,80 @@
+import os
+import configparser
+import image_slicer
+from shutil import copyfile
+import cv2
+
+config = configparser.ConfigParser()
+config.read('config.ini')
+
+inputMap = config['DEFAULT']['inputMap']
+rootTargetDirectory = config['DEFAULT']['rootTargetDirectory']
+zoomLevels = int(config['DEFAULT']['zoomLevels'])
+
+if not os.path.exists(rootTargetDirectory):
+    os.makedirs(rootTargetDirectory)
+
+baseFactor = 2
+originalBaseFactor = baseFactor
+
+
+
+def makeTiles():
+    global rootTargetDirectory, zoomLevels, baseFactor
+
+    for zl in range(1, zoomLevels):
+
+        rows = baseFactor
+        columns = baseFactor
+        slices = rows * columns
+
+        print("Processing - Zoom Level:"+str(zl)+" Slices:"+str(slices)+" Columns:"+str(columns)+" Rows:"+str(rows))
+
+        tiles = image_slicer.slice(inputMap, slices, columns, rows, False)
+        zlTargetDirectory = "tiles/"+str(zl)
+
+        #make zoomlevel directory if not exists
+        if not os.path.exists(zlTargetDirectory):
+            os.makedirs(zlTargetDirectory)
+
+        for tile in tiles:
+            currentTargetDirectory = rootTargetDirectory+"/"+str(zl)+"/"+str(tile.row-1)
+            if not os.path.exists(currentTargetDirectory):
+                os.makedirs(currentTargetDirectory)
+
+            tileFilename = str(tile.column-1)+".png";
+            #print(currentTargetDirectory+"/"+tileFilename)
+
+            tile.save(filename=currentTargetDirectory+"/"+tileFilename)
+            scaleTile(currentTargetDirectory+"/"+tileFilename)
+        
+
+        baseFactor = baseFactor * 2
+
+
+
+def scaleTile(tilePath):
+    img = cv2.imread(tilePath, cv2.IMREAD_UNCHANGED)
+    
+    scale_percent = 60 # percent of original size
+    width = 256
+    height = 256
+    dim = (width, height)
+    # resize image
+    resized = cv2.resize(img, dim, interpolation = cv2.INTER_AREA)
+    cv2.imwrite(tilePath, resized)
+
+
+#make zoom level 0! (which is just the entire map in 1 image)
+rootTilePath = "/0/0"
+if not os.path.exists(rootTargetDirectory+rootTilePath):
+        os.makedirs(rootTargetDirectory+rootTilePath)
+
+print("Creating root tile")
+copyfile(inputMap, rootTargetDirectory+rootTilePath+"/0.png")
+scaleTile(rootTargetDirectory+rootTilePath+"/0.png")
+
+print("Making tiles")
+makeTiles()
+
+print("DONE!")
